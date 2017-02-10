197761a336ae9b293595d6f12f3d6ecc610951ed
Diarmaid Joyce <diarmaidharte@gmail.com>
2017-02-10 13:48:10 +0000

    Merged all the conflicts from DJ1stBranch and dev

--- src/server/Bank.java
+++ src/server/Bank.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,21 +22,30 @@ public class Bank extends UnicastRemoteObject implements BankInterface {
 	private static String depositTransaction = "Deposit Transaction";
 
 	public Bank() throws RemoteException {
-
 		Account user1 = new Account("user1", "pass1");
 		Account user2 = new Account("user2", "pass2");
 		Account user3 = new Account("user3", "pass3");
 		accounts.add(user1);
 		accounts.add(user2);
 		accounts.add(user3);
-	
 	}
 	
 	public static void main(String args[]) throws Exception {
-
-		//Gotta do shit here !!
+		if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Bank";
+            BankInterface bank = new Bank();
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, bank);
+            System.out.println("Bank bound");
+        } catch (Exception e) {
+            System.err.println("Bank exception:");
+            e.printStackTrace();
+        }
 	}
-	
+
 	public void login(String username, String password) throws RemoteException, InvalidLogin {
 		for(Account a : accounts) {
 			if(a.getUsername().equals(username) && a.getPassword().equals(password)) {
@@ -44,7 +55,7 @@ public class Bank extends UnicastRemoteObject implements BankInterface {
 			}
 		}
 	}
-
+		
 	public void deposit(int account, int amount, long sessionID) throws RemoteException, InvalidSession {
 		try {
 			Account a = getAccount(account, sessionID);
